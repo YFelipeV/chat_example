b9ea743feeebe00933d4eeeced0ba6ed8bdc424d
YFelipeV <felipesegura250@gmail.com>
2025-02-11 17:35:59 -0500

    add

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({jsxRuntime: 'classic'})],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.jsx"), // 📌 Asegurar la entrada correcta
